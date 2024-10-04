6bc175f2d29d7b8f66ee6633249df0ff493ab8f4
karinahri <dolmatovakarina72@gmail.com>
2024-10-04 14:43:09 +0300

    second commit

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -7,6 +7,15 @@
 
 import UIKit
 
+enum ValidationError: Error {
+    case notOriginalWord
+    case notRealWord
+    case alreadyUsedWord
+    case tooShort
+    case notPossibleWord
+}
+
+
 class ViewController: UITableViewController {
     var allWords = [String]()
     var usedWords = [String]()
@@ -49,19 +58,19 @@ class ViewController: UITableViewController {
     @objc func promptForAnswer() {
         let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
         ac.addTextField()
-
+        
         let submitAction = UIAlertAction(title: "Submit", style: .default) {
             [weak self, weak ac] _ in
             guard let answer = ac?.textFields?[0].text else { return }
-            self?.submit(answer.lowercased())
+            self?.submit(answer: answer.lowercased())
         }
-
+        
         ac.addAction(submitAction)
         present(ac, animated: true)
     }
     
     
-    @objc func restartGame () {
+    @objc func restartGame() {
         let ac = UIAlertController(title: "Restart game?", message: nil, preferredStyle: .alert)
         let restartAction = UIAlertAction(title: "Restart", style: .default) {
             [weak self] _ in
@@ -72,34 +81,37 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
-    func submit(_ answer: String) {
+    func submit(answer: String) {
         let lowerAnswer = answer.lowercased()
+        do {
+            try checkPossible(word: lowerAnswer)
+            try checkOriginal(word: lowerAnswer)
+            try checkReal(word: lowerAnswer)
+            usedWords.insert(lowerAnswer, at: 0)
             
-
-        if isPossible(word: lowerAnswer) {
-            if isOriginal(word: lowerAnswer) {
-                if isReal(word: lowerAnswer) {
-                    usedWords.insert(lowerAnswer, at: 0)
-                    
-                    let indexPath = IndexPath(row: 0, section: 0)
-                    tableView.insertRows(at: [indexPath], with: .automatic)
-                    
-                    return
-                } else {
-                    showErrorMessage("Word not recognised", "You can't just make them up, you know!")
-                }
-            } else {
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+            
+        } catch let error as ValidationError {
+            switch error {
+            case .notOriginalWord:
                 showErrorMessage("Word not original", "Be more original!")
+            case .notRealWord:
+                showErrorMessage("Word not recognised", "You can't just make them up, you know!")
+            case .notPossibleWord:
+                showErrorMessage("Word not possible", "You can't spell that word from \"\(title!)\"")
+            case .alreadyUsedWord:
+                showErrorMessage("Word not original", "Be more original!")
+            case .tooShort:
+                showErrorMessage("Word not recognised", "You can't just make them up, you know!")
             }
-        } else {
-            guard let title = title?.lowercased() else { return }
-            showErrorMessage("Word not possible", "You can't spell that word from \"\(title)\"")
-                }
-            }
-
+        } catch {
+            
+        }
         
-
-    func showErrorMessage (_ title: String, _ message: String) {
+    }
+    
+    func showErrorMessage(_ title: String, _ message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
@@ -107,33 +119,35 @@ class ViewController: UITableViewController {
     
     
     
-    func isPossible(word: String) -> Bool {
-        guard var tempWord = title?.lowercased() else { return false }
-
+    func checkPossible(word: String) throws {
+        var tempWord = title!.lowercased()
+        
         for letter in word.lowercased() {
             if let position = tempWord.firstIndex(of: letter) {
                 tempWord.remove(at: position)
             } else {
-                return false
+                throw ValidationError.notPossibleWord
             }
         }
-
-        return true
     }
     
-    func isOriginal(word: String) -> Bool {
-        if word == title { return false }
-    
-        return !usedWords.contains(word.lowercased())
+    func checkOriginal(word: String) throws {
+        if word == title { throw ValidationError.notOriginalWord }
+        
+        guard !usedWords.contains(word.lowercased()) else {
+            throw ValidationError.alreadyUsedWord
+        }
     }
-
-    func isReal(word: String) -> Bool {
-        if word.count < 3 { return false }
+    
+    func checkReal(word: String) throws {
+        if word.count < 3 { throw ValidationError.tooShort }
         let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
+        let range = NSRange(location: 0, length: word.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-
-        return misspelledRange.location == NSNotFound
+        
+        if misspelledRange.location != NSNotFound {
+            throw ValidationError.notRealWord
+        }
     }
 }
 
